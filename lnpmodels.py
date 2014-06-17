15734b69defbd4713ba142fabd00dcef57e7fded
Andrew Adare <andrewadare@gmail.com>
2014-06-17 15:51:38 -0600

    Added curvature smoothing to prior

--- lnpmodels.py
+++ lnpmodels.py
@@ -38,6 +38,25 @@ def lnuniform(x, xref, alpha):
       return -np.inf
   return 0.
 
+def fd2(ndim, disc=None):
+  '''
+  Generate a square second-order finite-difference matrix.
+  The optional disc parameter excludes a discontinuity in the solution at
+  position disc.
+  '''
+  d = np.ones(ndim)
+  a = np.diag(d[:-1], k=-1) + np.diag(-2*d) + np.diag(d[:-1], k=1)
+  a[0,0] = a[-1,-1] = -1
+  if disc is None:
+    return a
+  q1, q2 = np.zeros((ndim,1)), np.zeros((ndim,1))
+  q1[0:disc:] = 1.
+  q2[disc::] = 1.
+  q1 *= 1./np.sqrt(np.dot(q1.T,q1))
+  q2 *= 1./np.sqrt(np.dot(q2.T,q2))
+  tmp = np.identity(ndim) - np.dot(q1,q1.T) - np.dot(q2,q2.T)
+  return np.dot(a, tmp)
+
 # (Log) posterior pdf functions for MCMC samplers
 # Designed for linear systems Ax = b where
 # A = transfer matrix
@@ -48,17 +67,37 @@ def lnuniform(x, xref, alpha):
 # Posterior is \propto prior*likelihood
 # -----------------------------------------------
 
-# Gaussian prior x Poisson likelihood
 def gaussian_poisson(x, A, b, icov_data, x_prior, icov_prior):
+  '''
+  Gaussian prior; Poisson likelihood
+  '''
   return lngauss(x,x_prior,icov_prior) + lnpoiss(b, np.dot(A,x))
 
-# Gamma prior x Poisson likelihood
 def gamma_poisson(x, A, b, icov_data, x_prior, gamma_a, gamma_b):
   '''
+  Gamma prior; Poisson likelihood
   x_prior is a vector of prior means.
   '''
   return lngamma(x, gamma_a, gamma_b) + lnpoiss(b, np.dot(A,x))
 
-# Gaussian prior x Gaussian likelihood
 def gaussian_gaussian(x, A, b, icov_data, x_prior, icov_prior):
+  '''
+  Gaussian prior; Gaussian likelihood
+  '''
   return lngauss(x,x_prior,icov_prior) + lngauss(b, np.dot(A,x), icov_data)
+
+def l2_poisson(x, A, b, icov_data, x_prior, alpha, xmin, xmax, L=None):
+  '''
+  2-norm of (L*) x/x_prior; Poisson likelihood
+  Useful options for L:
+  1. Unit matrix (standard-form Tikhonov regularization). Default if no L.
+  2. A smoothing matrix (like fd2 above) for general-form Tikhonov reg.  
+  '''
+  if np.any(x < xmin) or np.any(x > xmax):
+    return -np.inf
+
+  xr = x/x_prior
+  if L is not None:
+    xr = np.dot(L,xr)
+  return -alpha*alpha*np.dot(xr,xr) + lnpoiss(b, np.dot(A,x))
+
