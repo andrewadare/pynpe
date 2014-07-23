1d82dbfc0e483e0a678be400f50fb4a8de301bb5
Andrew Adare <andrewadare@gmail.com>
2014-07-23 11:52:49 -0600

    Almost complete rewrite using npe_io.py to get inputs. Also, include likelihood plots for monitoring/diagnostics

--- samplehpt.py
+++ samplehpt.py
@@ -3,132 +3,58 @@ samplehpt.py
 Sample from joint probability to find heavy-flavor hadron yields vs pt
 '''
 from __future__ import print_function
-from ROOT import TFile
-from h2np import h2a, binedges, binctrs
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm
 from matplotlib.colors import LogNorm
+from scipy.stats import norm
 from time import clock
 import emcee
 import triangle
-import sys
 import lnpmodels
+import raamodel
+import npe_io as io
 
-alpha = 10
+# Configuration
+alpha = 0.2
 nwalkers = 500
-nburnin = 500
+nburnin = 1000
 nsteps = 1000
+dtype = 'MC'
+dcares = 0.007  # 0.007 cm in MB Au+Au, 0.014 cm in p+p.
+bfrac = 0.007
 
-def draa(x):
-  return 1.3*np.sqrt(2*np.pi*1.1*1.1)*norm.pdf(x, loc=1.5, scale=1.1) + \
-  0.2/(1 + np.exp(-x+3))
+io.mf = io.modelfile(dcares, bfrac)
 
-def braa(x):
-  return 0.6*np.exp(-x/3) + \
-  1.1*np.sqrt(2*np.pi*1.5*1.5)*norm.pdf(x, loc=3.4, scale=1.5) + \
-  0.3/(1 + np.exp(-x+7))
+# Get data
+dca = None
+ept = None
+ept_err = None
+if dtype == 'MC':
+    ept = io.eptmatrix().sum(axis=1)
+    dca = [m.sum(axis=1) for m in io.dcamatrices()]
 
-dtype     = 'MC'
-dcabins   = (1.0, 1.5, 2.0, 2.5, 3.0, 4.0, 5.0)
-modelFile = TFile('rootfiles/bfrac0.030-dcares0um.root')
-eptFile   = TFile('rootfiles/simspectra.root')
-dcaFile   = TFile('rootfiles/simdca.root')
-hptGen    = eptFile.Get('hptGen')
-hAept     = modelFile.Get('hAept') # Transfer matrices from PYTHIA / DcaGen
-hEpt      = eptFile.Get('hEptMC') # Electron pt spectra
-dcaMat    = [None]*6
-measdca   = [None]*6
-measbkg   = [None]*6
+# Get model components
+eptmat, dcamat = io.matrices()
+gpt, gptx, gptbins = io.genpt()
+ndim = eptmat.shape[1]
+raa = raamodel.getraa(gptx, ndim / 2)
+gptmod = gpt * raa
+eptmod = np.dot(eptmat, gptmod)
+ept_err = np.sqrt(eptmod)
+dcamod = [np.dot(m, gptmod) for m in io.dcamatrices()]
+eptx, eptbins = io.eptbins()
 
-for i in range(6):
-  dcaMat[i] = h2a(modelFile.Get('hdca{}'.format(i)))
-  prefix = '' if dtype=='MC' else 'qm12'
-  measdca[i] = h2a(dcaFile.Get('{}{}dca{}'.format(prefix, dtype, i)))
-  # measbkg[i] = bkgFile.Get('{}{}bkg{}'.format(prefix, dtype, i))
-  # print measbkg[i].Integral()
-
-if False:
-  nr,nc = 2,3
-  fig, axes = plt.subplots(nr,nc)
-  for row in range(nr):
-    for col in range(nc):
-      i = nc*row + col
-      a = axes[row,col]
-      a.tick_params(axis='x', top='off', labelsize=4)
-      a.tick_params(axis='y', left='off', right='off', labelsize=4)
-      s = r'{0:.1f}-{1:.1f} GeV/c'.format(dcabins[i], dcabins[i+1])
-      a.text(0.6, 0.9, s, fontsize=5, transform=a.transAxes)
-      m = dcaMat[i]
-      p = a.pcolormesh(m, norm=LogNorm(vmin=m.min()+1e-8, vmax=m.max()),
-                       cmap='Spectral_r')
-      # fig.colorbar(p)
-  fig.savefig('pdfs/dca_matrices.pdf', bbox_inches='tight')
-
-# Convert histograms to numpy arrays
-aept    = h2a(hAept)
-hpt     = aept.sum(axis=0) #h2a(hptGen)
-ept     = aept.sum(axis=1) #h2a(hEpt)
-dca     = [m.sum(axis=1) for m in dcaMat]
-bkg     = [np.mean(d[0:10])*np.ones_like(d) for d in dca]
-hptx    = binctrs(hptGen,'x')
-eptx    = binctrs(hEpt,'x')
-dcax    = binctrs(modelFile.Get('hdca0'),'x')
-ndim    = aept.shape[1]
-raa     = np.concatenate((draa(hptx[:10]), braa(hptx[:10])))
-hptmod  = raa*hpt
-hptbins = binedges(hptGen,'x')
-
-aept /= aept.sum(axis=0)
-dcaMat = [m for m in dcaMat] # [m/m.sum(axis=0) for m in dcaMat]
-eptmod  = np.dot(aept,hptmod)
-dcamod  = [np.dot(m,hptmod) for m in dcaMat]
-ept_err = np.sqrt(eptmod)     #h2a(hEpt, 'e')
-
-if False:
-  nr,nc = 2,3
-  fig, axes = plt.subplots(nr,nc)
-  for row in range(nr):
-    for col in range(nc):
-      i = nc*row + col
-      a = axes[row,col]
-      a.set_yscale('log')
-      a.tick_params(axis='x', top='off', labelsize=6)
-      a.tick_params(axis='y', labelsize=6)
-      s = r'{0:.1f}-{1:.1f} GeV/c'.format(dcabins[i], dcabins[i+1])
-      a.text(0.55, 0.9, s, fontsize=8, transform=a.transAxes)
-      a.step(dcax, bkg[i], color='brown')
-      a.step(dcax, dca[i], color='black', alpha = 0.6)
-      a.step(dcax, dcamod[i], color='red', alpha = 0.6)
-      # a.errorbar(eptx, eptrefold, yerr= [eptref_lo, eptref_hi],
-      #         lw=2, ls='*', marker='s', ms=10, color='r')
-      # a.errorbar(eptx, eptmod, yerr=ept_err, lw=2, ls='*', marker='o', color='k')
-  
-      # fig.colorbar(p)
-  fig.savefig('pdfs/dca_dists.pdf', bbox_inches='tight')
-
-# sys.exit()
-
-# Ensemble of starting points for the walkers. 
+# Ensemble of starting points for the walkers.
 print("Initializing {} {}-dim walkers...".format(nwalkers, ndim))
-p0 = hpt*(1. + 0.1*np.random.randn(nwalkers, ndim))
+p0 = gpt * (1. + 0.1 * np.random.randn(nwalkers, ndim))
 
-# Gaussian prior, Poisson likelihood
-icov_data  = np.diagflat(1./ept_err**2)
-icov_prior = np.diagflat((alpha/hpt)**2)
-# sampler = emcee.EnsembleSampler(nwalkers, ndim, lnpmodels.gaussian_poisson, 
-#                                 args=[aept, eptmod, icov_data, hpt, icov_prior])
-
-# # Gamma prior, Poisson likelihood
-# sampler = emcee.EnsembleSampler(nwalkers, ndim, gamma_poisson, 
-#                                 args=[aept, ept, icov_data, 
-#                                 hpt, hpt/alpha, np.ones_like(hpt)/alpha])
-
+ymin = 0.01 * gpt
+ymax = 2 * gpt
 L = lnpmodels.fd2(ndim)
-ymin = 0.01*hpt
-ymax = 2*hpt
-sampler = emcee.EnsembleSampler(nwalkers, ndim, lnpmodels.l2_poisson, 
-                                args=[aept, eptmod, hpt, alpha, ymin, ymax, L])
+fcn = lnpmodels.l2_poisson
+args = [eptmat, eptmod, gpt, alpha, ymin, ymax, L]
+sampler = emcee.EnsembleSampler(nwalkers, ndim, fcn, args=args, threads=2)
 
 print("Burning in for {} steps...".format(nburnin), end=' ')
 sys.stdout.flush()
@@ -136,26 +62,24 @@ start = clock()
 pos, prob, state = sampler.run_mcmc(p0, nburnin)
 sampler.reset()
 end = clock()
-print(str(end-start) + ' s')
-print("state: {} len({}) {} {} {}".format(state[0], 
-      len(state[1]), state[2], state[3], state[4]))
+print(str(end - start) + ' s')
+# print("state: {} len({}) {} {} {}".format(state[0], len(state[1]), state[2], state[3], state[4]))
 
 print("Running sampler for {} steps...".format(nsteps), end=' ')
 sys.stdout.flush()
 start = clock()
 sampler.run_mcmc(pos, nsteps)
 end = clock()
-print(str(end-start) + ' s')
+print(str(end - start) + ' s')
 
 acc_frac = np.mean(sampler.acceptance_fraction)
 print("Mean acceptance fraction: {0:.3f}".format(acc_frac))
 
-# Flatten down the output
+# Initial shape is (nwalkers, nsteps, ndim). Reshape to (nwalkers*nsteps, ndim).
 samples = sampler.chain.reshape((-1, ndim))
-print(samples.shape)
 
 # Posterior quantiles: list of ndim (16,50,84) percentile tuples
-pq = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]), 
+pq = map(lambda v: (v[1], v[2] - v[1], v[1] - v[0]),
          zip(*np.percentile(samples, [16, 50, 84], axis=0)))
 pq = np.array(pq)
 
@@ -165,72 +89,80 @@ pq = np.array(pq)
 
 # Draw posterior marginal distributions
 # sample.chain has shape (nwalkers, nsteps, ndim)
-print("Histogramming results for plotting...")
-nr,nc = 4,5
-fig, axes = plt.subplots(nr,nc)
+print("Marginalizing posterior distribution for summary plot...")
+nr, nc = 4, 5
+fig, axes = plt.subplots(nr, nc)
 for row in range(nr):
-  for col in range(nc):
-    i = nc*row + col
-    a = axes[row,col]
-    fc = 'yellow' if i < ndim/2 else 'lime'
-    a.hist(sampler.flatchain[:,i], 100, 
-           color='k',facecolor=fc, histtype='stepfilled')
-    a.tick_params(axis='x', top='off', labelsize=4)
-    a.tick_params(axis='y', left='off', right='off', labelsize=0)
-    a.xaxis.get_offset_text().set_size(4)
-    a.xaxis.get_major_formatter().set_powerlimits((0, 1))
-    s = r'{0:.0f}-{1:.0f} GeV/c'.format(hptbins[i%10], hptbins[i%10+1])
-    a.text(0.6, 0.9, s, fontsize=5, transform=a.transAxes)
-    if i < ndim/2:
-      a.text(0.05, 0.9, r'$h_{charm}$', fontsize=5, transform=a.transAxes)
-    else:
-      a.text(0.05, 0.9, r'$h_{beauty}$', fontsize=5, transform=a.transAxes)
+    for col in range(nc):
+        i = nc * row + col
+        a = axes[row, col]
+        fc = 'yellow' if i < ndim / 2 else 'lime'
+        a.hist(sampler.flatchain[:, i], 100,
+               color='k', facecolor=fc, histtype='stepfilled')
+        a.tick_params(axis='x', top='off', labelsize=4)
+        a.tick_params(axis='y', left='off', right='off', labelsize=0)
+        a.xaxis.get_offset_text().set_size(4)
+        a.xaxis.get_major_formatter().set_powerlimits((0, 1))
+        s = r'{0:.0f}-{1:.0f} GeV/c'.format(
+            gptbins[i % 10], gptbins[i % 10 + 1])
+        a.text(0.6, 0.9, s, fontsize=5, transform=a.transAxes)
+        if i < ndim / 2:
+            a.text(
+                0.05, 0.9, r'$h_{charm}$', fontsize=5, transform=a.transAxes)
+        else:
+            a.text(
+                0.05, 0.9, r'$h_{beauty}$', fontsize=5, transform=a.transAxes)
 fig.savefig('pdfs/posterior.pdf', bbox_inches='tight')
+
+print("Plotting log probabilities...")
+fig, ax = plt.subplots()
+ax.set_xlabel('log likelihood distribution')
+ax.set_ylabel('samples')
+ax.hist(sampler.flatlnprobability, 100,
+        color='k', facecolor='lightyellow', histtype='stepfilled')
+fig.savefig('pdfs/lnprob.pdf')
+
+print("Plotting log probabilities vs step...")
+fig, ax = plt.subplots()
+ax.set_xlabel('step')
+# ax.set_ylabel(r'$\langle$log likelihood$\rangle$')
+ax.set_ylabel(r'$\langle \ln(L) \rangle_{chains}$')
+ax.set_title(r'$\langle \ln(L) \rangle_{chains}$ vs. sample step')
+ax.plot(np.sum(sampler.lnprobability/nwalkers, axis=0), color='k')
+ax.text(0.05, 0.95, '{} chains after {} burn-in steps'.format(nwalkers, nburnin),
+        transform=ax.transAxes)
+fig.savefig('pdfs/lnprob-vs-step.pdf')
 
 # Draw prior, walkers, initial guess, and result.
 print("Drawing results...")
-fig, axes = plt.subplots(1,2)
-ptx = hptx[:ndim/2]
+fig, axes = plt.subplots(1, 2)
+ptx = gptx[:ndim / 2]
 for ax in axes:
-  cb = 'charm' if ax == axes[0] else 'beauty'
-  r  = range(ndim/2) if ax == axes[0] else range(ndim/2,ndim)
-  ax.set_yscale('log')
-  ax.set_xlabel(r'{} hadron $p_T$ [GeV/c]'.format(cb))
-  # ymin and ymax (part of prior)
-  ax.fill_between(ptx, ymin[r], ymax[r], color='slategray', alpha=0.1)
-  # walkers
-  for i in range(nwalkers): 
-    ax.plot(ptx, p0[i,r], ls='*', marker='s', ms=14, color='deepskyblue', alpha=0.01)
-  # gen
-  ax.plot(ptx, hpt[r], lw=2, ls='*', marker='o', color='white')
-  # mod
-  ax.plot(ptx, hptmod[r], lw=2, ls='*', marker='s', color='black')
-  # result
-  ax.errorbar(ptx, pq[r,0], yerr=[pq[r,2], pq[r,1]],
-            ls='*', fmt='o', color='crimson', ecolor='crimson', capthick=2)
+    cb = 'c' if ax == axes[0] else 'b'
+    r = range(ndim / 2) if ax == axes[0] else range(ndim / 2, ndim)
+    ax.set_yscale('log')
+    ax.set_xlabel(r'{} hadron $p_T$ [GeV/c]'.format(cb))
+    ax.fill_between(ptx, ymin[r], ymax[r], color='slategray', alpha=0.1)
+    for i in range(nwalkers):
+        ax.plot(ptx, p0[i, r], ls='*', marker='s',
+                ms=14, color='deepskyblue', alpha=0.01)
+    ax.plot(ptx, gpt[r], lw=2, ls='*', marker='o', color='white')
+    ax.plot(ptx, gptmod[r], lw=2, ls='*', marker='s', color='black')
+    ax.errorbar(ptx, pq[r, 0], yerr=[pq[r, 2], pq[r, 1]],
+                ls='*', fmt='o', color='crimson', ecolor='crimson', capthick=2)
 
 fig.savefig('pdfs/hpt.pdf')
 
-# Draw matrix
-fig, ax = plt.subplots()
-p = ax.pcolormesh(aept, norm=LogNorm(vmin=aept.min()+1e-8, vmax=aept.max()), 
-                  cmap='Spectral_r') #cmap='RdYlBu_r') #cmap='Blues')
-ax.set_xlabel(r'h $p_T$ bin index')
-ax.set_ylabel(r'$e^{\pm}$ $p_T$ bin index')
-ax.set_ylim([0, aept.shape[1]+1])
-fig.colorbar(p)
-fig.savefig('pdfs/aept.pdf')
-
 # Draw ept
-eptrefold = np.dot(aept,pq[:,0])
-eptref_hi = np.dot(aept,pq[:,1])
-eptref_lo = np.dot(aept,pq[:,2])
+eptrefold = np.dot(eptmat, pq[:, 0])
+eptref_hi = np.dot(eptmat, pq[:, 1])
+eptref_lo = np.dot(eptmat, pq[:, 2])
 
 fig, ax = plt.subplots()
 ax.set_yscale('log')
 ax.set_xlabel(r'$e^{\pm}$ $p_T$ [GeV/c]')
 ax.errorbar(eptx, ept, yerr=ept_err, lw=2, ls='*', marker='o', color='white')
-ax.errorbar(eptx, eptrefold, yerr= [eptref_lo, eptref_hi],
+ax.errorbar(eptx, eptrefold, yerr=[eptref_lo, eptref_hi],
             lw=2, ls='*', marker='s', ms=10, color='r')
 ax.errorbar(eptx, eptmod, yerr=ept_err, lw=2, ls='*', marker='o', color='k')
 fig.savefig('pdfs/ept.pdf')
