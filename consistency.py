ae31a676ae64b01ba2c5bb94fca085b32dc778ba
Andrew Adare <andrewadare@gmail.com>
2014-10-29 15:06:22 -0600

    Move refold functions to new refold.py module

--- consistency.py
+++ consistency.py
@@ -11,8 +11,9 @@ import numpy as np
 from matrixplotter import mmplot
 import unfold_input as ui
 import plotting_functions as pf
+from refold import dca_refold, ept_refold
 
-np.set_printoptions(precision=3)
+# np.set_printoptions(precision=3)
 
 bfrac = 0.007
 # Decay matrices - elements are joint probabilities
@@ -24,39 +25,6 @@ gpt = ui.genpt(bfrac)
 ept_ideal = ui.eptmat_proj(bfrac, axis=1)
 dca_ideal = [ui.dcamat_proj(i, bfrac, axis=1) for i in range(6)]
 dca = [ui.dcadata_sim(i, bfrac) for i in range(6)]
-
-
-def dca_refold(gpt, dcamat, dca):
-    c, b = ui.idx['c'], ui.idx['b']
-    cfold, bfold, hfold = [], [], []
-    bfrac = np.zeros((len(dca),3))
-    for i, m in enumerate(dcamat):
-        cf = np.dot(m[:, c], gpt[c])
-        bf = np.dot(m[:, b], gpt[b])
-        hf = cf + bf
-        scf = dca[i][:, 0].sum() / hf[:, 0].sum()
-        cf *= scf
-        bf *= scf
-        hf *= scf
-        cfold.append(cf)
-        bfold.append(bf)
-        hfold.append(hf)
-        bfrac[i,:] = np.sum(bf) / np.sum(hf)
-
-    # Make errors zero until I find time to compute them correctly...
-    bfrac[:,1:] *= 0.
-    return cfold, bfold, hfold, bfrac
-
-
-def ept_refold(gpt, eptmat):
-    c, b = ui.idx['c'], ui.idx['b']
-    cfold = np.dot(eptmat[:, c], gpt[c])
-    bfold = np.dot(eptmat[:, b], gpt[b])
-    hfold = cfold + bfold
-    bfrac = bfold / hfold
-    # Make errors zero until I find time to compute them correctly...
-    bfrac[:,1:] *= 0.
-    return cfold, bfold, hfold, bfrac
 
 if True:
     cfold, bfold, hfold, _ = dca_refold(gpt, dcamat, dca)
